e877ecbdfffae3ddd33fcb80d4f4fab3d9ac8526
Joe Eagar <joeedh@gmail.com>
2026-03-28 03:34:21 +0000

    update readme

--- start-jypiter.sh
+++ start-jypiter.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p /workspaces/jypitercontainer/notebook
-jupyter lab --ip=0.0.0.0 --notebook-dir=/workspaces/jypitercontainer/notebook --port=8888 --no-browser --NotebookApp.token='' --NotebookApp.password='' &
+jupyter lab --ip=0.0.0.0 --notebook-dir=/workspaces/jypitercontainer/notebook --port=8888 --no-browser --NotebookApp.token='' --NotebookApp.password=''
