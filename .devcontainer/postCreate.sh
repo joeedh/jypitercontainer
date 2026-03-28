a9de15b9b848f9a9f4d6231809c76fe9eac2a736
Joe Eagar <joeedh@gmail.com>
2026-03-27 21:22:00 -0600

    remove ls

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,4 @@
 # setup git autocompletion and copy our slightly customized
 # .bashrc
-ls && cp .devcontainer/.bashrc /home/vscode
+cp .devcontainer/.bashrc /home/vscode
 cp .devcontainer/git-completion.bash /home/vscode
