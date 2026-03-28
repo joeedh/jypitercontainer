22fa145a4e1c639d037eab6a0df15b6deb04b29b
Joe Eagar <joeedh@gmail.com>
2026-03-28 04:47:11 +0000

    Create jupity config files. Also made start_jupiter.sh stop any running servers

--- .devcontainer/.bashrc
+++ .devcontainer/.bashrc
@@ -57,7 +57,7 @@ if [ -n "$force_color_prompt" ]; then
 fi
 
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\] \[\033[01;33m\](`git branch --show-current`)\[\033[00m\] :\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\] \[\033[38;5;208m\](`git branch --show-current`)\[\033[00m\] :\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
     PS1='${debian_chroot:+($debian_chroot)}\u:\w\$ '
 fi
@@ -114,3 +114,5 @@ fi
 
 source ~/git-completion.bash
 echo "Open Jupiter lab at http://localhost:8888"
+
+JUPYTER_CONFIG_DIR=/workspaces/jypitercontainer/.jupyter
--- start-jypiter.sh
+++ start-jypiter.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+# note: this is also set in vscode's .bashrc 
+export JUPYTER_CONFIG_DIR=`pwd`/.jupyter
+
+echo "Using jupyter config dir $JUPYTER_CONFIG_DIR"
+
+echo "Stopping server at 8888 if one is running"
+# stop any running servers
+jupyter server stop 8888
+
+# ensure user settings folders exists, set in config
+mkdir -p /workspaces/jypitercontainer/.jupyter/server/user-settings
+mkdir -p /workspaces/jypitercontainer/.jupyter/lab/user-settings
+
+# ensure the notebook folder exists
 mkdir -p /workspaces/jypitercontainer/notebook
-jupyter lab --ip=0.0.0.0 --notebook-dir=/workspaces/jypitercontainer/notebook --port=8888 --no-browser --NotebookApp.token='' --NotebookApp.password=''
+
+# start the sserver
+jupyter lab --ServerApp.token='0' --ip=0.0.0.0 --notebook-dir='/workspaces/jypitercontainer/notebook' --port=8888 
